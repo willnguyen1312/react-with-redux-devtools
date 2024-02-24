f0212a253cf541dbfd2ada43624d3b03cc58c378
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-02-24 12:38:55 -0400

    Finalize sample devtools 💞

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { setupDevtools } from "./devtools";
 
-function App() {
-  const [count, setCount] = useState(0)
+type State = {
+  value: number;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type Action = {
+  type: "incrementOne";
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "incrementOne":
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
-export default App
+type Subscriber = () => void;
+
+const createStore = (arg: {
+  reducer: (state: State, action: Action) => State;
+  initialState: State;
+}) => {
+  let state = arg.initialState;
+  const listeners: Set<Subscriber> = new Set();
+
+  const getState = () => state;
+
+  const setState = (newState: Partial<State>) => {
+    state = { ...state, ...newState };
+    listeners.forEach((listener) => listener());
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let devtoolsResult: any = undefined;
+
+  const dispatch = (action: Action) => {
+    state = arg.reducer(state, action);
+
+    devtoolsResult?.dispatch(action);
+
+    listeners.forEach((listener) => listener());
+  };
+
+  devtoolsResult = setupDevtools?.({
+    getState,
+    setState,
+    dispatch,
+  });
+
+  return {
+    dispatch,
+    getState,
+    setState,
+    subscribe: (subscriber: Subscriber) => {
+      listeners.add(subscriber);
+      return () => {
+        listeners.delete(subscriber);
+      };
+    },
+  };
+};
+
+export default function App() {
+  const storeRef = React.useRef<ReturnType<typeof createStore>>();
+
+  if (!storeRef.current) {
+    storeRef.current = createStore({
+      reducer,
+      initialState: {
+        value: 0,
+      },
+    });
+  }
+
+  const state = React.useSyncExternalStore(
+    storeRef.current.subscribe,
+    storeRef.current.getState
+  );
+
+  // debugger;
+  // console.log(state.value);
+
+  return (
+    <div>
+      <p>Value: {state.value}</p>
+
+      <button
+        onClick={() => {
+          storeRef.current!.dispatch({ type: "incrementOne" });
+        }}
+      >
+        Add one
+      </button>
+    </div>
+  );
+}
---
+++ src/devtools.ts
@@ -0,0 +1,115 @@
+// Credit - https://github.com/pmndrs/zustand/blob/6109bc3bd0f3850c2d9546956de971f27834ac7a/src/middleware/devtools.ts
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type {} from "@redux-devtools/extension";
+
+const isDEV = import.meta.env.MODE !== "production";
+
+type Message = {
+  type: string;
+  payload?: any;
+  state?: any;
+};
+
+type SetupDevtoolsConfig<T> = {
+  getState: () => T;
+  setState: (newState: Partial<T>) => void;
+  dispatch: (action: any) => void;
+};
+
+function setupDevtoolsImplementation<T>({
+  getState,
+  setState,
+  dispatch,
+}: SetupDevtoolsConfig<T>) {
+  const extensionConnector = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (!extensionConnector) {
+    console.warn("Please install/enable Redux devtools extension");
+    return;
+  }
+
+  const connection = extensionConnector.connect({});
+
+  const initialState = getState();
+
+  connection.init(initialState);
+
+  (
+    connection as unknown as {
+      subscribe: (
+        listener: (message: Message) => void
+      ) => (() => void) | undefined;
+    }
+  ).subscribe((message) => {
+    switch (message.type) {
+      case "ACTION":
+        return parseJsonThen(message.payload, (action) => {
+          dispatch(action);
+          return;
+        });
+
+      case "DISPATCH":
+        switch (message.payload.type) {
+          case "RESET":
+            setState(initialState);
+            return connection.init(getState());
+
+          case "COMMIT":
+            return connection.init(getState());
+
+          case "ROLLBACK":
+            return parseJsonThen(message.state, (state: any) => {
+              setState(state);
+              connection.init(getState());
+            });
+
+          case "JUMP_TO_STATE":
+          case "JUMP_TO_ACTION":
+            return parseJsonThen(message.state, (state: any) => {
+              setState(state);
+
+              if (state._meta?.stackTrace) {
+                console.clear();
+                console.log(state._meta.stackTrace);
+              }
+            });
+
+          case "IMPORT_STATE": {
+            const { nextLiftedState } = message.payload;
+            const lastComputedState =
+              nextLiftedState.computedStates.slice(-1)[0]?.state;
+            if (!lastComputedState) return;
+
+            setState(lastComputedState);
+            connection?.send(null as any, nextLiftedState);
+            return;
+          }
+        }
+        return;
+    }
+  });
+
+  return {
+    dispatch: (action: any) => {
+      const stackTrace = new Error("stack trace").stack ?? "";
+      connection.send(action, { ...getState(), _meta: { stackTrace } });
+    },
+  };
+}
+
+export const setupDevtools = isDEV ? setupDevtoolsImplementation : undefined;
+
+const parseJsonThen = <T>(
+  stringified: string,
+  callBack: (parsed: T) => void
+) => {
+  let parsed: T | undefined;
+  try {
+    parsed = JSON.parse(stringified);
+  } catch (e) {
+    console.error("Could not parse the received json", e);
+  }
+
+  if (parsed) callBack(parsed as T);
+};
